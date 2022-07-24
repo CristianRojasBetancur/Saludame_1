1756fb4e7e9120ed2bd7dcf1fdd18a1b5b1054ef
CristianRojasBetancur <66922367+CristianRojasBetancur@users.noreply.github.com>
2022-07-23 23:22:20 -0500

    Error solucionado

--- solution.rb
+++ solution.rb
@@ -2,13 +2,13 @@ require 'sinatra'
 
 get '/' do
 	# params[:nombre] ? "Hola #{params[:nombre]}" : "Hola desconocido!"
-	unless params[:nombre] || params[:nombre] =! ""
+	if params[:nombre] && params[:nombre] != ""
 		<<-HTML
-			<h1 style="text-align: center;">¡Hola desconocido!</h1>
+			<h1 style="text-align: center;">¡Hola #{params[:nombre]}!</h1>
 		HTML
 	else
 		<<-HTML
-			<h1 style="text-align: center;">¡Hola #{params[:nombre]}!</h1>
+			<h1 style="text-align: center;">¡Hola desconocido!</h1>
 		HTML
 	end
 end
